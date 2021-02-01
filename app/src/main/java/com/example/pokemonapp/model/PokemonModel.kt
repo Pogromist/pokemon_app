0976d33374bb71db626fffede8aa987cfa8fb3de
Pogromist <a.g.klyukin@gmail.com>
2021-02-01 21:20:49 +0300

    Created Pokemon Model

---
+++ app/src/main/java/com/example/pokemonapp/model/PokemonModel.kt
@@ -0,0 +1,10 @@
+package com.example.pokemonapp.model
+
+data class PokemonModel(
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val base_experience: Int,
+    val form: String
+)
