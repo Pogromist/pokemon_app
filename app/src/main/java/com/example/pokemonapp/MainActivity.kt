1536940e185c2e6a50b0e7c7a7d579087dc468bc
Pogromist <a.g.klyukin@gmail.com>
2021-02-04 17:44:18 +0300

    First version of navigation (for test).

--- app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.pokemonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.pokemonapp.presentation.pokemonlistscreen.PokemonListFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(PokemonListFragment())
+
+        bottomNavigationMenu.setOnNavigationItemReselectedListener {
+            when(it.itemId) {
+                R.id.pokemonListFragment -> replaceFragment(PokemonListFragment())
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
