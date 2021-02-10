9ca318f78b0d509f793cdc7014b1b6397a8ec820
Pogromist <a.g.klyukin@gmail.com>
2021-02-10 11:27:27 +0300

    Create Model

---
+++ app/src/main/java/com/example/pokemonapp/data/model/DetailPokemonData.kt
@@ -0,0 +1,10 @@
+package com.example.pokemonapp.data.model
+
+data class DetailPokemonData(
+    val id: Int? = null,
+    val name: String? = null,
+    val height: Int? = null,
+    val weight: Int? = null,
+    val base_experience: Int? = null,
+    val form: String? = null
+)
