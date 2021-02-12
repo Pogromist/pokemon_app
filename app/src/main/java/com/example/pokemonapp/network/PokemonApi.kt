abba452e9a9af239d006fcbe6bdd54a072eb7bcb
Pogromist <a.g.klyukin@gmail.com>
2021-02-12 11:26:47 +0300

    PokemonApi modified

--- app/src/main/java/com/example/pokemonapp/network/PokemonApi.kt
+++ app/src/main/java/com/example/pokemonapp/network/PokemonApi.kt
@@ -1,12 +1,17 @@
 package com.example.pokemonapp.network
 
-import com.example.pokemonapp.data.model.PokemonResponseData
+import com.example.pokemonapp.data.model.PokemonDetailData
+import com.example.pokemonapp.data.model.PokemonNameData
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
+
 
 interface PokemonApi {
 
     @GET("pokemon")
-    fun getPokemonList(): Observable<PokemonResponseData>
+    fun getPokemonList(): Observable<PokemonNameData>
 
+    @GET("pokemon/{id}")
+    fun getPokemonDetails(@Path("id") id: Int?): Observable<PokemonDetailData>
 }
