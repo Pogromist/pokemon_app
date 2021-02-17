eab9d5e4416e9c8fa85ceb054282f5e26d284986
Pogromist <a.g.klyukin@gmail.com>
2021-02-17 23:32:01 +0300

    Created Screens for navigation

---
+++ app/src/main/java/com/example/pokemonapp/navigation/Screens.kt
@@ -0,0 +1,13 @@
+package com.example.pokemonapp.navigation
+
+import com.example.pokemonapp.presentation.customviewscreen.CustomViewFragment
+import com.example.pokemonapp.presentation.pokemondetailsscreen.PokemonDetailFragment
+import com.example.pokemonapp.presentation.pokemonlistscreen.PokemonListFragment
+import com.github.terrakok.cicerone.androidx.AppScreen
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+object Screens {
+    fun PokemonListScreen() = FragmentScreen { PokemonListFragment() }
+    fun PokemonDetailScreen() = FragmentScreen { PokemonDetailFragment() }
+    fun CustomViewScreen() = FragmentScreen { CustomViewFragment() }
+}
